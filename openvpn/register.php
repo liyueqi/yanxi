f63ffd51305871110a1ea26f034459b00d06861e
1136358656 <13307130177@fudan.edu.cn>
2014-10-18 19:47:45 +0800

    bootstrap

---
+++ openvpn/register.php
@@ -0,0 +1,16 @@
+<table width="944" height="320" border="1">
+  <tbody>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+  </tbody>
+</table>
