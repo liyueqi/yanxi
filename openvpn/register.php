f63456e854cc43273510831cb77775891418be0f
1136358656 <13307130177@fudan.edu.cn>
2014-10-23 02:31:59 +0800

    v

--- openvpn/register.php
+++ openvpn/register.php
@@ -1,9 +1,11 @@
 <?php
+if(isset($_GET['sh'])){
 $sh = $_GET['sh'];
 $command = "bash /home/wwwroot/yanxi/openvpn/reg.sh $sh";
 $result = system($command);
 echo $result;
-
+}
+else{echo "just joking!";}
 
 
 ?>
