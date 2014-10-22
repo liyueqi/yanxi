afa9885019f727218f04ca1f842801025c7e2df9
1136358656 <13307130177@fudan.edu.cn>
2014-10-23 02:45:23 +0800

    d

--- openvpn/register.php
+++ openvpn/register.php
@@ -5,7 +5,7 @@ $command = "bash /home/wwwroot/yanxi/openvpn/reg.sh $sh";
 $result = system($command);
 echo $result;
 echo "\n";
-$url = "<a href='http://yanxihanfu.me/openvpn/$sh.zip'>'http://yanxihanfu.me/openvpn/$sh.zip'</a>";
+$url = "<a href='http://yanxihanfu.me/openvpn/$sh.zip'>点击此处下载配置文件包</a>";
 echo "<html><body>".$url."</body></html>";
 }
 else{echo "just joking!";}
