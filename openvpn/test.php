3b9be2f68b059890f8e38ae78e81777a1bc1726b
1136358656 <13307130177@fudan.edu.cn>
2014-10-24 02:11:34 +0800

    s

---
+++ openvpn/test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>≤‚ ‘</title>
+	 <link href="http://yanxihanfu.me/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	 <link href="http://yanxihanfu.me/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+	 <link rel="stylesheet" href="http://yanxihanfu.me/bootstrap/css/bootstrap-theme.min.css">
+	<script src="http://yanxihanfu.me/bootstrap/js/jquery.min.js"></script>
+	<script src="http://yanxihanfu.me/bootstrap/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+	<button type="button" class="btn btn-lg btn-default">Default</button>
+	
+	
+	</body>
+	
+	</html>
+
