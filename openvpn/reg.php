8acfabf3c5cbcf0d54f5fb15a5e9e5f9e99de09a
1136358656 <13307130177@fudan.edu.cn>
2014-10-24 19:41:32 +0800

    y

--- openvpn/reg.php
+++ openvpn/reg.php
@@ -1,4 +1,69 @@
 <!DOCTYPE html>
+<?php
+class mysql
+{
+    public $Conn;
+    private $sql;
+    private $dbStatus;
+    private $RawResult;
+    private $Result;
+    public function __construct(){
+        $this->Conn = mysql_connect("localhost","root","1136358656");
+        mysql_select_db("openvpn") or die(mysql_error());
+    }
+    public function SelectDb($db)
+    {
+        $this->dbStatus = mysql_select_db($db,$this->Conn);
+        mysql_query("set names gb2312",$this->Conn);
+        return $this->dbStatus;
+    }
+    public function query($query)
+    {
+        $this->RawResult = mysql_query($query,$this->Conn);
+        //$this->Result = mysql_fetch_row($this->RawResult);
+        return $this->RawResult;
+    }
+    public function GetConn(){
+        return $this->Conn;
+    }
+}
+if(isset($_POST['code']))
+{
+    $code = $_POST['code'];
+    $conn = new mysql();
+    $sql = "select * from invitecode where code='$code'";
+    $result = $conn->query($sql);
+    $rows = mysql_fetch_row($result);
+    $num = count($rows);
+    $time = date('Y-m-d H:i:s',time());
+    $timeshot = strtotime($time);
+    do{
+    $cername = substr(crypt($timeshot),0,8);
+    $sql = "select * from invitecode where cert='$cername'";
+    $result = $conn->query($sql);
+    $rows = mysql_fetch_row($result);
+    $count = count($rows);
+    }while($count != 0);
+    if($num = 0){
+        echo "<script>alert('该注册码无效！'); </script>";
+    }else{
+        $sql = "update invitecode set status='已使用',regtime='$time',cert='$cername' where code='$code'";
+        $result = $conn->query($sql);
+
+    }
+
+
+}else{
+}
+
+
+
+
+
+
+?>
+
+
 <html>
   <head>
     <meta charset="gbk">
--- openvpn/register.php
+++ openvpn/register.php
@@ -2,7 +2,7 @@
 if(isset($_GET['sh'])){
 $sh = $_GET['sh'];
 $command = "bash /home/wwwroot/yanxi/openvpn/reg.sh $sh";
-$result = system($command,$x);
+$result = shell_exec($command);
 //echo $result;
 echo "<br>";
 $url = "<a href='http://yanxihanfu.me/openvpn/$sh.zip'>Click here to download the keys!</a>";
