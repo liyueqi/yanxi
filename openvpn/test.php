8e2e1409a8e4a183c9389f7f74e4b374dd58e641
1136358656 <13307130177@fudan.edu.cn>
2014-10-24 14:10:58 +0800

    xc

--- openvpn/test.php
+++ openvpn/test.php
@@ -12,13 +12,28 @@
 	<script src="http://yanxihanfu.me/bootstrap/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-	<div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">主页</a></li>
-            <li><a href="#about">注册</a></li>
-            <li><a href="#contact">试用</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Jelly VPN</a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">主页</a></li>
+              <li><a href="#about">注册</a></li>
+              <li><a href="#contact">试用</a></li>
+              <li><a href="#contact">联系我们</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </nav>
+	
 	
 	
 	</body>
