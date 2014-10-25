ba1d91f2068a39a040fae3c301f8aa8e08bb813f
1136358656 <1136358656@qq.com>
2014-10-26 02:20:22 +0800

    d

---
+++ openvpn/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+
+
+<html>
+
+  <head>
+    <meta charset="gbk">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Jelly VPN</title>
+	 <link href="http://yanxihanfu.me/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	 <link href="http://yanxihanfu.me/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+	 <link rel="stylesheet" href="http://yanxihanfu.me/bootstrap/css/bootstrap-theme.min.css">
+	<script src="http://yanxihanfu.me/bootstrap/js/jquery.min.js"></script>
+	<script src="http://yanxihanfu.me/bootstrap/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Jelly VPN</a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="index.php">主页</a></li>
+              <li><a href="#">注册</a></li>
+              <li><a href="try.php">试用</a></li>
+              <li><a href="contact.php">联系我们</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </nav>
+    <div class="well">
+        <p><h4>本VPN走IPv6线路，IPv6下有效，</h4></p>
+        <p><h4>服务器位于纽约。</h4></p>
+    </div>
+    
+	</body>
+	
+	</html>
+
