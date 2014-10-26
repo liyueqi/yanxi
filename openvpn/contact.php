c8635d37ac80800842f57879b2c0099ec36427ac
1136358656 <1136358656@qq.com>
2014-10-27 03:18:53 +0800

    w

--- openvpn/contact.php
+++ openvpn/contact.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
+<?php
+header("Content-type: text/html; charset=utf-8"); 
+?>
 <html>
   <head>
-    <meta charset="gbk">
+    
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Jelly VPN</title>
