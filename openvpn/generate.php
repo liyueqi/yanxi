0a4f72499bab2d96639921add969da3c3f311084
1136358656 <1136358656@qq.com>
2014-10-28 15:36:42 +0800

    e

--- openvpn/generate.php
+++ openvpn/generate.php
@@ -90,7 +90,7 @@ if(isset($_POST['code']))
     while($len >= 8){
         $res[$i] = substr($code,0,8);
         echo $res[$i];
-        $code = str_replace($res[$i],"",$code);
+        $code = str_replace($res[$i]," ",$code);
         $i = $i + 1;
     }
 
