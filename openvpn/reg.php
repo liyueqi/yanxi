1d97b62c321c8276a75963cfceb0df7297512789
1136358656 <1136358656@qq.com>
2014-11-02 16:46:04 +0800

    eww

--- openvpn/reg.php
+++ openvpn/reg.php
@@ -93,7 +93,7 @@
     </nav>
     
     
-    <div style="position: absolute; background: #DA70D6; left: 404px; top: 53px;filter: alpha(opacity=90);opacity: 0.9;">
+    <div style="width: 900px; position: absolute; background: #DA70D6; left: 225px; top: 53px; filter: alpha(opacity=70); opacity: 0.7;">
         <div class="well">
         <p><h3 align="center">本VPN走IPv6线路，IPv6下有效，</h3></p>
         <p align="center"><h3 align="center">服务器位于纽约。</h3></p>
