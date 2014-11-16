df157f54acb59141bb1e84c3c38854d4940cdcd1
1136358656 <1136358656@qq.com>
2014-11-16 16:51:44 +0800

    w

--- openvpn/boot/header.php
+++ openvpn/boot/header.php
@@ -1,4 +1,3 @@
-﻿
 <!DOCTYPE html>
 
 <!-- 
@@ -18,6 +17,7 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
 <!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
 
 <!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+
 <!--[if !IE]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
 
 <!-- BEGIN HEAD -->
@@ -120,9 +120,109 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
 
 						<i class="icon-warning-sign"></i>
 
-						
+						<span class="badge">6</span>
 
 						</a>
+
+						<ul class="dropdown-menu extended notification">
+
+							<li>
+
+								<p>You have 14 new notifications</p>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="label label-success"><i class="icon-plus"></i></span>
+
+								New user registered. 
+
+								<span class="time">Just now</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="label label-important"><i class="icon-bolt"></i></span>
+
+								Server #12 overloaded. 
+
+								<span class="time">15 mins</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="label label-warning"><i class="icon-bell"></i></span>
+
+								Server #2 not respoding.
+
+								<span class="time">22 mins</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="label label-info"><i class="icon-bullhorn"></i></span>
+
+								Application error.
+
+								<span class="time">40 mins</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="label label-important"><i class="icon-bolt"></i></span>
+
+								Database overloaded 68%. 
+
+								<span class="time">2 hrs</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="label label-important"><i class="icon-bolt"></i></span>
+
+								2 user IP blocked.
+
+								<span class="time">5 hrs</span>
+
+								</a>
+
+							</li>
+
+							<li class="external">
+
+								<a href="#">See all notifications <i class="m-icon-swapright"></i></a>
+
+							</li>
+
+						</ul>
 
 					</li>
 
@@ -354,4 +454,6 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
 
 			<!-- END SAMPLE PORTLET CONFIGURATION MODAL FORM-->
 
-			<!-- BEGIN PAGE CONTAINER-->	
+			<!-- BEGIN PAGE CONTAINER-->
+
+			
