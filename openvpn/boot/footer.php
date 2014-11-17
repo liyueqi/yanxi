0782f7b8678f0e4d2971ca675faa49effcae7420
1136358656 <1136358656@qq.com>
2014-11-17 16:03:14 +0800

    we

--- openvpn/boot/footer.php
+++ openvpn/boot/footer.php
@@ -1,5 +1,5 @@
 <?php
-echo '<hr>
-<div align="center">Powered By Kaguya <br /><script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253526699'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s23.cnzz.com/z_stat.php%3Fid%3D1253526699%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script></div>
+echo "<hr>
+<div align=".'center">'."Powered By Kaguya <br /><scri".'pt type="text/javascript">var cnzz_protocol ='. '(("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan i'."d='"."cnzz_stat_icon_1253526699'".'%3E%3C/span%3E%3Cscrip'."t src='".' " '.'+ cnzz_protocol + "s23.cnzz.com/z_stat.php%3Fid%3D1253526699%26show%3D'."pic' type='text/javascri".'p'."t'".'%3E%3C/script%3E"));</script></div>
 </div>';
 ?>
