5ab77b86f4a1adc48e4b69b7aa27482ff7150ca2
1136358656 <1136358656@qq.com>
2014-11-19 01:46:20 +0800

    sd

--- openvpn/boot/logout.php
+++ openvpn/boot/logout.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type:text/html; charset=utf-8');
 $url = $_SERVER['HTTP_REFERER'];
-setcookie("jellystatus","");
+setcookie("jellystatus",'',time()-3600,"/");
 echo '<script>alert("已注销登录！");</script>';
 header("location: $url");
 ?>
