6050f941701eae7be4e7461c47e94f0e1d38c30a
1136358656 <1136358656@qq.com>
2014-11-19 02:23:46 +0800

    mj

--- openvpn/boot/password-change.php
+++ openvpn/boot/password-change.php
@@ -124,15 +124,7 @@ include 'header.php';
 
 							<li class="pull-right no-text-shadow">
 
-								<div id="dashboard-report-range" class="dashboard-date-range tooltips no-tooltip-on-touch-device responsive" data-tablet="" data-desktop="tooltips" data-placement="top" data-original-title="Change dashboard date range">
-
-									<i class="icon-calendar"></i>
-
-									<span></span>
-
-									<i class="icon-angle-down"></i>
-
-								</div>
+								
 
 							</li>
 
