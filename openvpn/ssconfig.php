2a48142c64d92f43f3be55056ae18700334d2d75
Kaguya <1136358656@qq.coom>
2015-01-25 19:26:43 +0800

    2

---
+++ openvpn/ssconfig.php
@@ -0,0 +1,22 @@
+<?php
+$IP = $_GET['ip'];
+$port = $_GET['port'];
+$passwd = $_GET['passwd'];
+$method = $_GET['method'];
+echo "\{\n
+\"configs\" \: \[\n
+  \{\n
+\"server\" \: \"$IP\",\n
+\"server_port\" \: $port\,\n
+\"password\" \: \"$passwd\"\,\n
+\"method\" \: \"$method\"\,\n
+\"remarks\" \: \"\"\}\n\n
+\]\,\n
+\"index\" \: 0\,\n
+\"global\" \: true\,\n
+\"enabled\" \: \false\,\n
+\"shareOverLan\" \: false\,\n
+\"isDefault\" \: false\,\n
+\"localPort\" \: 1080\}"\n
+
+?>
