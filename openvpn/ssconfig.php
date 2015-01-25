7e03483e9f5256b3dcac276a8c0507c1be83a86e
Kaguya <1136358656@qq.coom>
2015-01-25 19:40:52 +0800

    w

--- openvpn/ssconfig.php
+++ openvpn/ssconfig.php
@@ -3,20 +3,20 @@ $IP = $_GET['ip'];
 $port = $_GET['port'];
 $passwd = $_GET['passwd'];
 $method = $_GET['method'];
-echo "\{\n
-\"configs\" \: \[\n
-  \{\n
-\"server\" \: \"$IP\",\n
-\"server_port\" \: $port\,\n
-\"password\" \: \"$passwd\"\,\n
-\"method\" \: \"$method\"\,\n
-\"remarks\" \: \"\"\}\n\n
-\]\,\n
-\"index\" \: 0\,\n
-\"global\" \: true\,\n
-\"enabled\" \: \false\,\n
-\"shareOverLan\" \: false\,\n
-\"isDefault\" \: false\,\n
-\"localPort\" \: 1080\}\n"
+echo "{\n
+\"configs\" : [\n
+  {\n
+\"server\" : \"$IP\",\n
+\"server_port\" : $port,\n
+\"password\" : \"$passwd\",\n
+\"method\" : \"$method\",\n
+\"remarks\" : \"\"}\n\n
+],\n
+\"index\" : 0,\n
+\"global\" : true,\n
+\"enabled\" : \false,\n
+\"shareOverLan\" : false,\n
+\"isDefault\" : false,\n
+\"localPort\" : 1080}\n"
 
 ?>
