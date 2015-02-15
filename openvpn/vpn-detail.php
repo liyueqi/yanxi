285a3cdce8043920df3f7aeef9ccf8f71c938a7a
Kaguya <1136358656@qq.coom>
2015-02-15 19:44:33 +0800

    w

--- openvpn/vpn-detail.php
+++ openvpn/vpn-detail.php
@@ -5,7 +5,9 @@ include('header.php');
 ?>
 <body>
 <div class="col-md-3 ">
-        <div id="toc" class="bc-sidebar"><ul class="nav"><li class="toc-h2 toc-active"><a href="#toc0">项目说明</a></li><li class="toc-h2"><a href="#toc1">费用说明</a></li>
+        <div id="toc" class="bc-sidebar"><ul class="nav">
+        <li class="toc-h2 toc-active"><a href="#toc0">项目说明</a></li>
+        <li class="toc-h2"><a href="#toc1">费用说明</a></li>
         <li class="toc-h2"><a href="#toc2">使用教程</a></li>
         <li class="toc-h2"><a href="#toc3">UnBlock Youku 使用教程</a></li>
     <li class="toc-h2"><a href="#toc4">FAQ</a></li></ul></div>
@@ -18,7 +20,24 @@ include('header.php');
             <p><h3>基本介绍</h3></p>
             <p><h4>现在一般大学内基本都有免费的<a href="http://zh.wikipedia.org/zh-hk/IPv6">IPv6</a>网络，但是<a href="http://zh.wikipedia.org/wiki/IPv4">IPv4</a>的网络连接往往是收费的（也就是所谓的“外网”）。本项目的目的是提供一个基于<a href="http://zh.wikipedia.org/zh-hk/IPv6">IPv6</a>的廉价、稳定、自由的网络连接，通过本项目提供的服务，您可以完美解决上述问题，节省上网费用，自由访问任意网站。</h4></p>
             <p><h4>我们拥有自己开发的专用客户端，功能丰富且实用，无论您是否熟悉网络配置，均可简便地通过我们提供的客户端使用服务。</h4></p>
-            
+            <span id="toc1"></span><h2 id="">费用说明<div align="right"><a href="#"><img src="./openvpn-pic/top.jpg" width="76" height="18"/></a>
+                </div></h2>
+                <p><h4>VPN的当前价格为￥10.00/月，其中包含：</h4></p>
+<blockquote>
+<h4>
+<p>IPv6下有效的VPN使用权</p>
+<p>IPv4下有效的shadowsocks和Goagent PaaS使用权</p>
+</h4>
+<div class="alert alert-danger" role="alert">注意：本项目提供的OpenVPN账号仅在IPv6环境下有效，在IPv4环境下无效！(shadowsocks和Goagent PaaS不受影响)请在购买前仔细确认，恕不接受由于无IPv6而提出的退款要求！谢谢合作！</div>
+</blockquote>
+<p><h4>免费试用</h4></p>
+<blockquote>
+<h4>
+<p>用于免费试用的VPN配置文件有效期为48小时，逾期自动作废</p>
+<p>包含IPv6下有效的VPN使用权和IPv4下Shadowsocks使用权</p>
+
+</h4>
+</blockquote>
             
             
             </section>
