66bc6a4f0d1eb70e0789d8d634656b49cdcbbe0e
Kaguya <1136358656@qq.coom>
2015-02-15 16:04:53 +0800

    w

--- openvpn/index.php
+++ openvpn/index.php
@@ -11,20 +11,20 @@
         <div class="col-lg-4">
           <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
           <h2>IPv6 OpenVPN代理</h2>
-          <p><h4>现在一般大学内基本都有免费的<a href="http://zh.wikipedia.org/zh-hk/IPv6">IPv6</a>网络，但是<a href="http://zh.wikipedia.org/wiki/IPv4">IPv4</a>的网络连接往往是收费的（也就是所谓的“外网”）。本项目的目的是提供一个基于<a href="http://zh.wikipedia.org/zh-hk/IPv6">IPv6</a>的廉价、稳定、自由的网络连接，通过本项目提供的服务，您可以完美解决上述问题，实现节省上网费用，自由访问任意网站的目的。</h4></p>
+          <p><h5>现在一般大学内基本都有免费的<a href="http://zh.wikipedia.org/zh-hk/IPv6">IPv6</a>网络，但是<a href="http://zh.wikipedia.org/wiki/IPv4">IPv4</a>的网络连接往往是收费的（也就是所谓的“外网”）。本项目的目的是提供一个基于<a href="http://zh.wikipedia.org/zh-hk/IPv6">IPv6</a>的廉价、稳定、自由的网络连接，通过本项目提供的服务，您可以完美解决上述问题，节省上网费用，自由访问任意网站。</h5></p>
           <p><a class="btn btn-default" href="contact.php" role="button">详情 »</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
           <h2>IPv4 Shadowsocks代理</h2>
-          <p><h4>在大陆，由于<a href="http://zh.wikipedia.org/wiki/%E9%98%B2%E7%81%AB%E9%95%BF%E5%9F%8E">GFW</a>的封锁与干扰，很多的国外网站都无法正常的访问，除了Facebook、twitter、YouTube以外，还有大量的学术网站，而各种VPN正在被重点打击，越来越不稳定。本项目的目的是提供一个稳定、轻量、跨平台的代理方案，实现自由访问互联网。</h4></p>
+          <p><h5>在大陆，由于<a href="http://zh.wikipedia.org/wiki/%E9%98%B2%E7%81%AB%E9%95%BF%E5%9F%8E">GFW</a>的封锁与干扰，很多的国外网站都无法正常的访问，除了Facebook、twitter、YouTube以外，还有大量的学术网站，而各种VPN正在被重点打击，越来越不稳定。本项目的目的是提供一个稳定、轻量、跨平台的代理方案，实现自由访问互联网。</h5></p>
           <p><a class="btn btn-default" href="#" role="button">详情 »</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
           <h2>PHP 虚拟主机</h2>
-          <p>有很多用户想建立自己的个人网站、博客，但是苦于不熟悉架设流程，本项目的目的是提供一个廉价、稳定、简便的网站架设平台，并提供必要的技术支持。</p>
-          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+          <p><h5>有很多用户想建立自己的个人网站、博客，但是苦于不熟悉架设流程，本项目的目的是提供一个廉价、稳定、简便的网站架设平台，并提供必要的技术支持。</h5></p>
+          <p><a class="btn btn-default" href="#" role="button">详情 »</a></p>
         </div><!-- /.col-lg-4 -->
       </div>
 
