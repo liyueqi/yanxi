5d55b8e132078bc6590cc6ebd8088818fc604ce6
Kaguya <1136358656@qq.coom>
2015-02-15 19:28:53 +0800

    h

--- openvpn/vpn-detail.php
+++ openvpn/vpn-detail.php
@@ -13,8 +13,11 @@ include('header.php');
 <div class="col-md-9">
         <article class="post page">
         	<section class="post-content">
-            
-            
+            <span id="toc0"></span><h2 id="">项目说明<div align="right"><a href="#"><img src="./openvpn-pic/top.jpg" width="76" height="18"/></a>
+                </div></h2>
+            <p><h3>基本介绍</h3></p>
+            <p><h4>现在一般大学内基本都有免费的<a href="http://zh.wikipedia.org/zh-hk/IPv6">IPv6</a>网络，但是<a href="http://zh.wikipedia.org/wiki/IPv4">IPv4</a>的网络连接往往是收费的（也就是所谓的“外网”）。本项目的目的是提供一个基于<a href="http://zh.wikipedia.org/zh-hk/IPv6">IPv6</a>的廉价、稳定、自由的网络连接，通过本项目提供的服务，您可以完美解决上述问题，节省上网费用，自由访问任意网站。</h4></p>
+            <p><h4>我们拥有自己开发的专用客户端，功能丰富且实用，无论您是否熟悉网络配置，均可简便地通过我们提供的客户端使用服务。</h4></p>
             
             
             
