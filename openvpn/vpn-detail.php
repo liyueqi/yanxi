c90d73c0250891b1e16f9e0d880227e0a34b63c5
Kaguya <1136358656@qq.coom>
2015-02-15 17:41:55 +0800

    s

---
+++ openvpn/vpn-detail.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<?php
+include('header.php');
+?>
+<body>
+<div class="col-md-3 ">
+        <div id="toc" class="bc-sidebar"><ul class="nav"><li class="toc-h2 toc-active"><a href="#toc0">项目说明</a></li><li class="toc-h2"><a href="#toc1">费用说明</a></li>
+        <li class="toc-h2"><a href="#toc2">使用教程</a></li>
+        <li class="toc-h2"><a href="#toc3">UnBlock Youku 使用教程</a></li>
+    <li class="toc-h2"><a href="#toc4">FAQ</a></li></ul></div>
+	  </div>
+<div class="col-md-9">
+        <article class="post page">
+        	<section class="post-content">
+            
+            
+            
+            
+            
+            </section>
+            </article>
+        
+      </div>
+
+<?php
+include('footer.php');
+?>
+</body>
+</html>
