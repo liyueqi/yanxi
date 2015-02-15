0d6ba35d44ad287a2cc49e4321bd7fa2acc5e971
Kaguya <1136358656@qq.coom>
2015-02-15 14:53:10 +0800

    e

--- openvpn/header.php
+++ openvpn/header.php
@@ -1,7 +1,6 @@
-﻿<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+<?php
+echo '  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Jelly VPN</title>
@@ -35,7 +34,7 @@
                   <li><a href="#">介绍&使用教程</a></li>
                   <li><a href="#">试用</a></li>
                   <li><a href="#">购买</a></li>
-                  
+
                 </ul>
               </li>
                <li class="dropdown">
@@ -45,10 +44,10 @@
                   <li><a href="#">介绍&使用教程</a></li>
                   <li><a href="#">试用</a></li>
                   <li><a href="#">购买</a></li>
-                  
+
                 </ul>
               </li>
-              
+
               <li><a href="contact.php">关于</a></li>
             </ul>
           </div><!--/.nav-collapse -->
@@ -58,46 +57,9 @@
   <div class="container">
     <h1>Jelly VPN</h1>
     <p class="lead">一个基于IPv6的openVPN代理</p>
-	
+
   </div>
 
-</header>
-<div class="carousel slide" id="carousel-61168">
-<ol class="carousel-indicators">
-	<li class="active" data-slide-to="0" data-target="#carousel-61168">&nbsp;</li>
-	<li data-slide-to="1" data-target="#carousel-61168">&nbsp;</li>
-	<li data-slide-to="2" data-target="#carousel-61168">&nbsp;</li>
-</ol>
+</header>  ';
 
-<div class="carousel-inner">
-<div class="item active"><img alt="" src="./openvpn-pic/jelly-1.jpg" />
-<div class="carousel-caption" contenteditable="true">
-
-</div>
-</div>
-
-<div class="item"><img alt="" src="./openvpn-pic/jelly-1.jpg" />
-<div class="carousel-caption" contenteditable="true">
-
-</div>
-</div>
-
-<div class="item"><img alt="" src="./openvpn-pic/jelly-1.jpg" />
-<div class="carousel-caption" contenteditable="true">
-
-</div>
-</div>
-</div>
-<a class="left carousel-control" data-slide="prev" href="#carousel-61168">&lsaquo;</a> <a class="right carousel-control" data-slide="next" href="#carousel-61168">&rsaquo;</a></div>
-
-<hr>
-<div align="center">Powered By Kaguya & Xiao </div> 
-<div align="center">2014 <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253526699'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s23.cnzz.com/z_stat.php%3Fid%3D1253526699%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script></div> 
-<div id=ipv6_enabled_www_test_logo></div>
-<script language="JavaScript" type="text/javascript">
-    	var Ipv6_Js_Server = (("https:" == document.location.protocol) ? "https://" : "http://");
-	document.write(unescape("%3Cscript src='" + Ipv6_Js_Server + "www.ipv6forum.com/ipv6_enabled/sa/SA.php?id=4893' type='text/javascript'%3E%3C/script%3E"));
-</script>    
-	</body>
-	</html>
-
+?>
---
+++ openvpn/index-test.php
@@ -0,0 +1,44 @@
+﻿<!DOCTYPE html>
+<html>
+<?php
+include('header.php');
+?>
+<div class="carousel slide" id="carousel-61168">
+<ol class="carousel-indicators">
+	<li class="active" data-slide-to="0" data-target="#carousel-61168">&nbsp;</li>
+	<li data-slide-to="1" data-target="#carousel-61168">&nbsp;</li>
+	<li data-slide-to="2" data-target="#carousel-61168">&nbsp;</li>
+</ol>
+
+<div class="carousel-inner">
+<div class="item active"><img alt="" src="./openvpn-pic/jelly-1.jpg" />
+<div class="carousel-caption" contenteditable="true">
+
+</div>
+</div>
+
+<div class="item"><img alt="" src="./openvpn-pic/jelly-1.jpg" />
+<div class="carousel-caption" contenteditable="true">
+
+</div>
+</div>
+
+<div class="item"><img alt="" src="./openvpn-pic/jelly-1.jpg" />
+<div class="carousel-caption" contenteditable="true">
+
+</div>
+</div>
+</div>
+<a class="left carousel-control" data-slide="prev" href="#carousel-61168">&lsaquo;</a> <a class="right carousel-control" data-slide="next" href="#carousel-61168">&rsaquo;</a></div>
+
+<hr>
+<div align="center">Powered By Kaguya & Xiao </div> 
+<div align="center">2014 <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253526699'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s23.cnzz.com/z_stat.php%3Fid%3D1253526699%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script></div> 
+<div id=ipv6_enabled_www_test_logo></div>
+<script language="JavaScript" type="text/javascript">
+    	var Ipv6_Js_Server = (("https:" == document.location.protocol) ? "https://" : "http://");
+	document.write(unescape("%3Cscript src='" + Ipv6_Js_Server + "www.ipv6forum.com/ipv6_enabled/sa/SA.php?id=4893' type='text/javascript'%3E%3C/script%3E"));
+</script>    
+	</body>
+	</html>
+
