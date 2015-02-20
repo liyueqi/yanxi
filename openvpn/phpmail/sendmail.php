1943ea3bf88e8abce44ee5bc4b51a0521e18209e
Kaguya <1136358656@qq.coom>
2015-02-20 22:24:29 +0800

    d

--- openvpn/phpmail/sendmail.php
+++ openvpn/phpmail/sendmail.php
@@ -1,4 +1,5 @@
 <?php
+header("Content-type: text/html; charset=utf-8"); 
     function send_mail() {
       $url = 'http://sendcloud.sohu.com/webapi/mail.send.json';
       //不同于登录SendCloud站点的帐号，您需要登录后台创建发信子帐号，使用子帐号和密码才可以进行邮件的发送。
