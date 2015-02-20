6c12a3d96e2877af7ecba2fba3154f0e2ebe70aa
Kaguya <1136358656@qq.coom>
2015-02-20 22:04:25 +0800

    s

--- openvpn/phpmail/sendmail.php
+++ openvpn/phpmail/sendmail.php
@@ -2,9 +2,9 @@
     function send_mail() {
       $url = 'http://sendcloud.sohu.com/webapi/mail.send.json';
       //不同于登录SendCloud站点的帐号，您需要登录后台创建发信子帐号，使用子帐号和密码才可以进行邮件的发送。
-      $param = array('api_user' => 'Synapse_test_JkHxVp',
+      $param = array('api_user' => 'synapsewebservicemail',
               'api_key' => 'WWhy6msPpXlqf2Fr',
-              'from' => 'service@sendcloud.im',
+              'from' => 'admin@yanlei.me',
               'fromname' => 'SendCloud测试邮件',
               'to' => '1136358656@qq.com',
               'subject' => '来自SendCloud的第一封邮件！',
@@ -17,4 +17,5 @@
       return $result;
     }
   echo send_mail();
+  var_dump($param);
 ?>
