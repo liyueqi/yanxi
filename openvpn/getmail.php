bf61395cf1144694e47c48362beea1a13bde28ee
Kaguya <1136358656@qq.coom>
2015-02-22 16:15:33 +0800

    z

--- openvpn/getmail.php
+++ openvpn/getmail.php
@@ -1,4 +1,14 @@
 <?php
-$arr=array("Volvo");
-echo json_encode($arr);
+if(isset($_GET['id'])){
+
+}else{
+    echo "<html>
+<head><title>401 Unauthorized</title></head>
+<body bgcolor=\"white\">
+<center><h1>401 Unauthorized</h1></center>
+<hr><center>nginx</center>
+</body>
+</html>
+";
+}
 ?>
