06b75c5c5ed600d10761a54da643ff1b8bac6bed
Kaguya <1136358656@qq.coom>
2015-02-22 16:21:26 +0800

    s

--- openvpn/getmail.php
+++ openvpn/getmail.php
@@ -1,5 +1,18 @@
 <?php
+include("mysql.php");
+
 if(isset($_GET['id'])){
+    $mailbox=$_GET['id'];
+    $conn = new mysql();
+    $sql = sprintf("select count(*) from users where mail='%s'and active='1'",
+        mysql_real_escape_string($mailbox));
+    $result = $conn->query($sql);
+    $rows = mysql_fetch_array($result);
+    $num = $rows[0];
+    if($num !=0){
+        echo "该邮箱已被占用";
+    }else{}
+
 
 }else{
     echo "<html>
