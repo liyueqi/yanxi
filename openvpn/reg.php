3aa6f265062fc535154bcbe1c4a614fb579a73f0
Kaguya <1136358656@qq.coom>
2015-03-04 02:00:50 +0800

    gh

--- openvpn/reg.php
+++ openvpn/reg.php
@@ -6,7 +6,7 @@ include("logo.php");
 include("mysql.php");
 ?>
 
-
+<body class="post-template page">
 
     <div id="in1" style="background: rgba(255, 255, 255, 0.4) !important;width: 70%; height:100%;overflow:hidden;margin:0 auto;opacity: 0.9; "><script>
 	  document.getElementById("in1").style.height=document.getElementById("in2").scrollHeight+"px"
@@ -71,33 +71,7 @@ include("mysql.php");
           }
       </script>  
     <?php
-    class mysql
-    {
-        public $Conn;
-        private $sql;
-        private $dbStatus;
-        private $RawResult;
-        private $Result;
-        public function __construct(){
-            $this->Conn = mysql_connect("localhost","root","1136358656");
-            mysql_select_db("openvpn") or die(mysql_error());
-        }
-        public function SelectDb($db)
-        {
-            $this->dbStatus = mysql_select_db($db,$this->Conn);
-            mysql_query("set names gb2312",$this->Conn);
-            return $this->dbStatus;
-        }
-        public function query($query)
-        {
-            $this->RawResult = mysql_query($query,$this->Conn);
-            //$this->Result = mysql_fetch_row($this->RawResult);
-            return $this->RawResult;
-        }
-        public function GetConn(){
-            return $this->Conn;
-        }
-    }
+
     if(isset($_POST['code']))
     {
         $code = $_POST['code'];
