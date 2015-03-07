0a71d209a5995d9c75280568ed21a32da63f252c
Kaguya <1136358656@qq.coom>
2015-03-07 23:51:23 +0800

    w

--- fuckgfw/logo.php
+++ fuckgfw/logo.php
@@ -3,7 +3,7 @@ echo '<header class="jumbotron masthead">
 
   <div class="container">
     <h1>Synapse for Hasi</h1>
-    <p class="lead" align="center">这是为蛤丝们提供的一个免费翻墙服务，本项目基于Shadowsocks,属于实验性项目。<br />服务器租用以及软件的后续开发维护暂时都由个人负担，不知道能够支撑到什么时候。<br />所以，还是欢迎愿意提供帮助的蛤丝们按照自己的意愿给本人捐款，来让这个项目更好的发展下去~</p>
+    <p class="lead" ><h4>这是为蛤丝们提供的一个免费翻墙服务，本项目基于Shadowsocks,属于实验性项目。<br />服务器租用以及软件的后续开发维护暂时都由个人负担，不知道能够支撑到什么时候。<br />所以，还是欢迎愿意提供帮助的蛤丝们按照自己的意愿给本人捐款，来让这个项目更好的发展下去~<h4></p>
 
   </div>
 
