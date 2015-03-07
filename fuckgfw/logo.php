f2dd01acfa04ce62e236f4252aef3dbfe819791f
Kaguya <1136358656@qq.coom>
2015-03-08 00:43:13 +0800

    s

--- fuckgfw/logo.php
+++ fuckgfw/logo.php
@@ -6,10 +6,9 @@ echo '<header class="jumbotron masthead">
     <p class="lead" ><h3>这是为蛤丝们提供的一个免费翻墙服务，本项目基于Shadowsocks,属于实验性项目。<br />服务器租用以及软件的后续开发维护暂时都由个人负担，不知道能够支撑到什么时候。<br />所以，还是欢迎愿意提供帮助的蛤丝们按照自己的意愿给本人捐款，来让这个项目更好的发展下去~<h3></p>
 	<p class="masthead-button-links">
           <a class="btn btn-lg btn-primary btn-shadow" href="./download/synapse.zip" target="_blank" role="button">下载客户端</a>
-        </p>
-	<p class="masthead-button-links">
-          <a class="btn btn-lg btn-info btn-shadow" href="./download/synapse.zip" target="_blank" role="button">我来捐助</a>
-        </p>	
+			<a class="btn btn-lg btn-info btn-shadow" href="./download/synapse.zip" target="_blank" role="button">我来捐助</a>
+		</p>
+	
   </div>
 
 </header>';
