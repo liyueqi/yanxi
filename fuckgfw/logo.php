d9a19f39c23b12e9058e3a853a2715fc4667d063
Kaguya <1136358656@qq.coom>
2015-03-07 23:46:28 +0800

    w

--- fuckgfw/logo.php
+++ fuckgfw/logo.php
@@ -1,8 +1,8 @@
 <?php
 echo '<header class="jumbotron subhead">
   <div class="container">
-    <h1>Synapse Web Service</h1>
-    <p class="lead">一个廉价、简便、全面的网络服务，包括代理服务器、网站虚拟主机等服务。</p>
+    <h1>Synapse for Hasi</h1>
+    <p class="lead" align="center">这是为蛤丝们提供的一个免费翻墙服务，本项目基于Shadowsocks,属于实验性项目。<br />服务器租用以及软件的后续开发维护暂时都由个人负担，不知道能够支撑到什么时候。<br />所以，还是欢迎愿意提供帮助的蛤丝们按照自己的意愿给本人捐款，来让这个项目更好的发展下去~</p>
 
   </div>
 
