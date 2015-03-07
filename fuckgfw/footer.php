87755b159b140976baa9b4b14c3651c29016d0dd
Kaguya <1136358656@qq.coom>
2015-03-08 02:58:15 +0800

    w

--- fuckgfw/footer.php
+++ fuckgfw/footer.php
@@ -1,19 +1,4 @@
 <?php
-echo '<hr>
-<div align="center">Powered By Kaguya</div>
-<div align="center">©2015 Synapse Web Service <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan ';
-echo "id='cnzz_stat_icon_1253526699'%3E%3C/span%3E%3Cscript src=' ";
-echo '" + cnzz_protocol + "s23.cnzz.com/z_stat.php%3Fid%3D1253526699%26show%3Dpic ';
-echo "' type='text/javascript'%3E%3C/script%3E ";
-echo '"));</script></div>
-<div id=ipv6_enabled_www_test_logo></div>
-<script language="JavaScript" type="text/javascript">
-    	var Ipv6_Js_Server = (("https:" == document.location.protocol) ? "https://" : "http://");
-	document.write(unescape("%3Cscript src=  ';
-echo "' ";
-echo '" + Ipv6_Js_Server + "www.ipv6forum.com/ipv6_enabled/sa/SA.php?id=4893 ';
-echo "' type='text/javascript'%3E%3C/script%3E ";
-echo '"));
-</script>';
+echo "<script type=\"text/javascript\">var cnzz_protocol = ((\"https:\" == document.location.protocol) ? \" https://\" : \" http://\");document.write(unescape(\"%3Cspan id=\'cnzz_stat_icon_1254504429\'%3E%3C/span%3E%3Cscript src=\'\" + cnzz_protocol + \"s4.cnzz.com/z_stat.php%3Fid%3D1254504429%26show%3Dpic\' type=\'text/javascript\'%3E%3C/script%3E\"));</script>";
 
 ?>
