528ac5bad7a53fe1aa13b32a70814cbccd8c970c
Kaguya <1136358656@qq.coom>
2015-03-08 19:51:54 +0800

    g

--- fuckgfw/donate-list.php
+++ fuckgfw/donate-list.php
@@ -5,17 +5,17 @@
   include('header.php');
   ?>
   <head>
-  <style type="text/css"> .divcss5{margin:0 auto;width:80%;height:150px;border:1px solid #000} </style></head>
+  <style type="text/css"> .divcss5{margin:0 auto;width:70%;height:150px;border:0px solid #000} </style></head>
 <div align="center"><p><h3>非常感谢蛤丝们对我的支持，我会把这个项目继续发展，完善的~~</h3></p></div>
 <div align="center" class="divcss5"><table class="table table-striped table-bordered table-hover" >
 <tr>
 <th>捐助者</th><th>数额</th><th>日期</th>
 </tr>
 <tr>
-<td>wy*********m</td>2.00<td></td><td>2015/03/07</td>
+<td>wy*********m</td><td>5.00</td><td>2015/03/07</td>
 </tr>
 <tr>
-<td>13********9</td>10.00<td></td><td>2015/03/08</td>
+<td>13********9</td><td>10.00</td><td>2015/03/08</td>
 </tr>
 
 
