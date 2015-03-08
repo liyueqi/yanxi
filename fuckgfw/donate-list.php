6e3b31290b23d593a3750967f3f1ffd65352a76e
Kaguya <1136358656@qq.coom>
2015-03-08 19:43:10 +0800

    w

--- fuckgfw/donate-list.php
+++ fuckgfw/donate-list.php
@@ -5,6 +5,20 @@
   
   ?>
 <div align="center"><p><h3>非常感谢蛤丝们对我的支持，我会把这个项目继续发展，完善的~~</h3></p></div>
+<div align="center"><table class="table table-striped table-bordered table-hover" >
+<tr>
+<th>捐助者</th><th>数额</th><th>日期</th>
+</tr>
+<tr>
+<td>wy*********m</td>2.00<td></td><td>2015/03/07</td>
+</tr>
+<tr>
+<td>13********9</td>10.00<td></td><td>2015/03/08</td>
+</tr>
+
+
+
+</table></div>
 <?php
 include('footer.php');
 ?>    
