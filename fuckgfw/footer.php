0286fa50be42a6771b3c88b1d83ac9e3390c2c01
Kaguya <1136358656@qq.coom>
2015-03-16 00:45:04 +0800

    q

--- fuckgfw/footer.php
+++ fuckgfw/footer.php
@@ -1,5 +1,5 @@
 <?php
-echo "<div align="center">友情链接：&nbsp;&nbsp;&nbsp;<a href="http://laohasi.com/" target="_blank">老蛤丝--蛤丝论坛</></div>";
+echo "<div align=\"center\">友情链接：&nbsp;&nbsp;&nbsp;<a href=\"http://laohasi.com/\" target=\"_blank\">老蛤丝--蛤丝论坛</></div>";
 echo '<hr>
 <div align="center">Powered By Kaguya</div>
 <div align="center">©2015 Synapse Web Service ';
