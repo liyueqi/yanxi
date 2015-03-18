7a049188632f59117bdc3741b18ba5cd76ef9bf3
Kaguya <1136358656@qq.coom>
2015-03-18 19:38:58 +0800

    w

--- fuckgfw/footer.php
+++ fuckgfw/footer.php
@@ -4,11 +4,9 @@ echo "<h4><div align=\"center\">友情链接：&nbsp;&nbsp;&nbsp;<a href=\"http:
 echo '<hr>
 <div align="center">Powered By Kaguya</div>
 <div align="center">©2015 Synapse Web Service ';
-echo "<div id=ipv6_enabled_www_test_logo></div>
-<script language=\"JavaScript\" type=\"text/javascript\">
-    	var Ipv6_Js_Server = ((\"http:\" == document.location.protocol) ? \"https://\" : \"http://\");
-	document.write(unescape(\"%3Cscript src=\'\" + Ipv6_Js_Server + \"www.ipv6forum.com/ipv6_enabled/sa/SA.php?id=4893\' type=\'text/javascript\'%3E%3C/script%3E\"));
-</script>";
+echo "<!-- IPv6-test.com button BEGIN -->
+<a href='http://ipv6-test.com/validate.php?url=referer'><img src='http://ipv6-test.com/button-ipv6-big.png' alt='ipv6 ready' title='ipv6 ready' border='0' /></a>
+<!-- IPv6-test.com button END -->";
 
 echo "<script type=\"text/javascript\">var cnzz_protocol = ((\"https:\" == document.location.protocol) ? \" https://\" : \" http://\");document.write(unescape(\"%3Cspan id=\'cnzz_stat_icon_1254504429\'%3E%3C/span%3E%3Cscript src=\'\" + cnzz_protocol + \"s4.cnzz.com/z_stat.php%3Fid%3D1254504429%26show%3Dpic\' type=\'text/javascript\'%3E%3C/script%3E\"));</script></div>";
 
