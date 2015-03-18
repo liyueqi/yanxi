8c19d333c905acbad77c3ac48bf1fde96b96d2a8
Kaguya <1136358656@qq.coom>
2015-03-18 19:28:32 +0800

    q

--- fuckgfw/footer.php
+++ fuckgfw/footer.php
@@ -9,6 +9,7 @@ echo "<div id=ipv6_enabled_www_test_logo></div>
     	var Ipv6_Js_Server = ((\"https:\" == document.location.protocol) ? \"https://\" : \"http://\");
 	document.write(unescape(\"%3Cscript src=\'\" + Ipv6_Js_Server + \"www.ipv6forum.com/ipv6_enabled/sa/SA.php?id=4893\' type=\'text/javascript\'%3E%3C/script%3E\"));
 </script>";
+
 echo "<script type=\"text/javascript\">var cnzz_protocol = ((\"https:\" == document.location.protocol) ? \" https://\" : \" http://\");document.write(unescape(\"%3Cspan id=\'cnzz_stat_icon_1254504429\'%3E%3C/span%3E%3Cscript src=\'\" + cnzz_protocol + \"s4.cnzz.com/z_stat.php%3Fid%3D1254504429%26show%3Dpic\' type=\'text/javascript\'%3E%3C/script%3E\"));</script></div>";
 
 ?>
