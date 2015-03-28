87019eb41f8605ffb9fa679b5659cbcfe3a3dbde
Kaguya <1136358656@qq.com>
2015-03-28 20:48:49 +0800

    Update footer.php

--- fuckgfw/footer.php
+++ fuckgfw/footer.php
@@ -3,7 +3,7 @@ echo "<br /><hr>";
 echo "<h4><div align=\"center\">友情链接：&nbsp;&nbsp;&nbsp;<a href=\"http://laohasi.com/\" target=\"_blank\">老蛤丝--蛤丝论坛</a></div></h4>";
 echo '<hr>
 <div align="center">Powered By Kaguya</div>
-<div align="center">©2015 Synapse Web Service ';
+<div align="center">©2015 Synapse Web Service <br />';
 echo "<!-- IPv6-test.com button BEGIN -->
 <a href='https://ipv6-test.com/validate.php?url=referer'><img src='./button-ipv6-big.png' alt='ipv6 ready' title='ipv6 ready' border='0' /></a>
 <!-- IPv6-test.com button END -->";
