757ecc1b21529e003fba8190e095cb88237ca48c
kaguya <1136358656@qq.com>
2015-04-05 15:29:00 +0800

    new begining

--- fuckgfw/testdes.php
+++ fuckgfw/testdes.php
@@ -1,10 +1,50 @@
 <?php
-require_once("./des.php");
-$key = "e537bfa04fef8b9e6b29e66a61620ef6";
-$Des = new Des();
-$config = "{ \"configs\" : [ { \"server\" : \"162.243.133.125\", \"server_port\" : 89, \"password\" : \"kaguya\", \"method\" : \"aes-128-cfb\", \"remarks\" : \"\"} ], \"index\" : 0, \"global\" : true, \"enabled\" : false, \"shareOverLan\" : false, \"isDefault\" : false, \"localPort\" : 1080}";
-$encode = $Des->encrypt($config,$key,true);
-$decode = $Des->decrypt($encode,$key,true);
+/*
+ * rc4加密算法
+ * $pwd 密钥
+ * $data 要加密的数据
+ */
+$str = "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqeweeeeeeeeeeeeeeeeee";
+$key = "8fe5fc78a95fbde89b7fa300be95fb47";
+$encode = rc4($key,$str);
+echo $encode;
+function rc4 ($pwd, $data)//$pwd密钥　$data需加密字符串
+{
+    $key[] ="";
+    $box[] ="";
 
-echo $config."|".$encode."|".$decode;
+    $pwd_length = strlen($pwd);
+    $data_length = strlen($data);
+    $cipher = "";
+
+    for ($i = 0; $i < 256; $i++)
+    {
+        $key[$i] = ord($pwd[$i % $pwd_length]);
+        $box[$i] = $i;
+    }
+
+    for ($j = $i = 0; $i < 256; $i++)
+    {
+        $j = ($j + $box[$i] + $key[$i]) % 256;
+        $tmp = $box[$i];
+        $box[$i] = $box[$j];
+        $box[$j] = $tmp;
+    }
+
+    for ($a = $j = $i = 0; $i < $data_length; $i++)
+    {
+        $a = ($a + 1) % 256;
+        $j = ($j + $box[$a]) % 256;
+
+        $tmp = $box[$a];
+        $box[$a] = $box[$j];
+        $box[$j] = $tmp;
+
+        $k = $box[(($box[$a] + $box[$j]) % 256)];
+        $cipher .= chr(ord($data[$i]) ^ $k);
+    }
+
+    return $cipher;
+}
+
 ?>
