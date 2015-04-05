69f7e4ad77159bce7ffe1c71ce69b3df20abde2f
kaguya <1136358656@qq.com>
2015-04-05 14:49:55 +0800

    new begining

--- fuckgfw/testdes.php
+++ fuckgfw/testdes.php
@@ -2,7 +2,7 @@
 require_once("./des.php");
 $key = "e537bfa04fef8b9e6b29e66a61620ef6";
 $Des = new Des();
-$config = "{ \"configs\" : [ { \"server\" : \"162.243.133.125\", \"server_port\" : 84, \"password\" : \"kaguya\", \"method\" : \"aes-128-cfb\", \"remarks\" : \"\"} ], \"index\" : 0, \"global\" : true, \"enabled\" : false, \"shareOverLan\" : false, \"isDefault\" : false, \"localPort\" : 1080}";
+$config = "{ \"configs\" : [ { \"server\" : \"162.243.133.125\", \"server_port\" : 89, \"password\" : \"kaguya\", \"method\" : \"aes-128-cfb\", \"remarks\" : \"\"} ], \"index\" : 0, \"global\" : true, \"enabled\" : false, \"shareOverLan\" : false, \"isDefault\" : false, \"localPort\" : 1080}";
 $encode = $Des->encrypt($config,$key,true);
 $decode = $Des->decrypt($encode,$key,true);
 
