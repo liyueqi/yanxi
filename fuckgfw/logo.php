ef6e7f1c131593f99d22d8a49ade41b280b38151
kaguya <1136358656@qq.com>
2015-04-09 20:56:23 +0800

    add bootstrap v3

--- fuckgfw/logo.php
+++ fuckgfw/logo.php
@@ -3,7 +3,7 @@ $img= "./images/282f729c837fe16209eefe7e9eeecd7b.jpg";
 echo "
 <style>
 .jumbotron{
-	background:url($img) no-repeat;
+	background:url($img); background-size:cover;
 }
 </style>
 <div align='center' class=\"jumbotron\">
