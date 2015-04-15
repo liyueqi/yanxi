8a1f0ee9abbba84c44d7908bd745820527a46bfa
kaguya <1136358656@qq.com>
2015-04-16 02:38:20 +0800

    add buttons

--- fuckgfw/index.php
+++ fuckgfw/index.php
@@ -5,7 +5,7 @@
   include ('logo.php');
   ?>
   <div align="center" style="width:75%;margin-left: auto;margin-right: auto;">
-  <div align="center" class="alert alert-danger" role="alert"><p><h4><strong>警告：</strong>据相关报告，360系列产品存在主动上报翻墙软件等恶意行为！请在使用本翻墙工具的时候务必关闭360系列软件以及一些国产安全软件</h4></p></div>
+  <div align="center" class="alert alert-danger" role="alert"><p><h4><strong>警告：</strong>据相关报告，360系列产品存在主动上报翻墙软件等恶意行为！<br />请在使用本翻墙工具的时候务必关闭360系列软件以及一些国产安全软件</h4></p></div>
   </div>
 <?php
 include('footer.php');
