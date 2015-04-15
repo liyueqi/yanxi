8217e3e2dc58340f461fc544e32569182950fc89
kaguya <1136358656@qq.com>
2015-04-16 02:44:25 +0800

    add buttons

--- fuckgfw/footer.php
+++ fuckgfw/footer.php
@@ -5,7 +5,7 @@ echo '<hr>
 <div align="center">Powered By Kaguya</div>
 <div align="center">©2015 Synapse Web Service <br />';
 echo "<!-- IPv6-test.com button BEGIN -->
-<a href='https://ipv6-test.com/validate.php?url=referer'><img src='./button-ipv6-big.png' alt='ipv6 ready' title='ipv6 ready' border='0' /></a>
+<a href='http://ipv6-test.com/validate.php?url=referer' target='_blank'><img src='./button-ipv6-big.png' alt='ipv6 ready' title='ipv6 ready' border='0' /></a>
 <!-- IPv6-test.com button END -->";
 
 echo "<script type=\"text/javascript\">var cnzz_protocol = ((\"https:\" == document.location.protocol) ? \" https://\" : \" http://\");document.write(unescape(\"%3Cspan id=\'cnzz_stat_icon_1254504429\'%3E%3C/span%3E%3Cscript src=\'\" + cnzz_protocol + \"s4.cnzz.com/z_stat.php%3Fid%3D1254504429%26show%3Dpic\' type=\'text/javascript\'%3E%3C/script%3E\"));</script></div>";
