f91169e4b93c25e7e75122b1da59800b553a7fa0
kaguya <1136358656@qq.com>
2015-04-16 02:51:48 +0800

    add buttons

--- openvpn/footer.php
+++ openvpn/footer.php
@@ -6,14 +6,11 @@ echo "id='cnzz_stat_icon_1253526699'%3E%3C/span%3E%3Cscript src=' ";
 echo '" + cnzz_protocol + "s23.cnzz.com/z_stat.php%3Fid%3D1253526699%26show%3Dpic ';
 echo "' type='text/javascript'%3E%3C/script%3E ";
 echo '"));</script></div>
-<div id=ipv6_enabled_www_test_logo></div>
-<script language="JavaScript" type="text/javascript">
-    	var Ipv6_Js_Server = (("https:" == document.location.protocol) ? "https://" : "http://");
-	document.write(unescape("%3Cscript src=  ';
-echo "' ";
-echo '" + Ipv6_Js_Server + "www.ipv6forum.com/ipv6_enabled/sa/SA.php?id=4893 ';
-echo "' type='text/javascript'%3E%3C/script%3E ";
-echo '"));
-</script>';
+';
+echo "<div id=ipv6_enabled_www_test_logo></div>
+<script language=\"JavaScript\" type=\"text/javascript\">
+    	var Ipv6_Js_Server = ((\"https:\" == document.location.protocol) ? \"https://\" : \"http://\");
+	document.write(unescape(\"%3Cscript src=\'\" + Ipv6_Js_Server + \"www.ipv6forum.com/ipv6_enabled/sa/SA.php?id=4893\' type=\'text/javascript\'%3E%3C/script%3E\"));
+</script>";
 
 ?>
