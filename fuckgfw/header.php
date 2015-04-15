81ed3a76a8d9932cfdb548c752889020bfa5a35d
kaguya <1136358656@qq.com>
2015-04-15 20:18:28 +0800

    move some resources to SAE

--- fuckgfw/header.php
+++ fuckgfw/header.php
@@ -6,6 +6,10 @@ $dirJs = "https://kaguyablog.sinaapp.com/synapse-cdn/bootstrap/js/bootstrap.js";
 $dirJquery = "https://kaguyablog.sinaapp.com/synapse-cdn/bootstrap/js/jquery.js";
 echo "  <head>
     <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
+    <meta name=\"description\"
+    content=\"这是为蛤丝们提供的一个免费翻墙服务，本项目基于Shadowsocks,属于实验性项目\">
+    <meta name=\"keywords\"
+    content=\"synapse,hasi,蛤丝,翻墙,科学上网,蛤丝论坛,shadowsocks,梯子\">
     <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
 	<title>Synapse for Hasi</title>
